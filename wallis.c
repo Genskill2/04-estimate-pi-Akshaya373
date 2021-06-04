36894afad7cbb310d4ce9a5e224beaab4654323a
Akshaya373 <84952190+Akshaya373@users.noreply.github.com>
2021-06-04 13:16:23 +0530

    Update wallis.c

--- wallis.c
+++ wallis.c
@@ -3,7 +3,17 @@
 #include <stdio.h>
 #include <math.h>
 
-float wallis_pi(int);
+float wallis_pi(int n)
+{float pi=1.0;
+for (int i=1;i<=n; i++)
+  {
+  int m;
+  m=4.0*i*i;
+  pi=(float)((pi*m)/(m-1));
+}
+  pi=pi *2.0;
+    return pi;
+}
 
 int main(void) {
   float pi;
@@ -22,5 +32,3 @@ int main(void) {
       abort();
     }
   }
-}
-
